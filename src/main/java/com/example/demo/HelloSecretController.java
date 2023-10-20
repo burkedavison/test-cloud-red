34ead94f89e156127f4461233e38357ed644f96a
Artem Bondar <artem.bondar@gmail.com>
2023-10-20 17:47:30 -0500

    jip plugin update

--- src/main/java/com/example/demo/HelloSecretController.java
+++ src/main/java/com/example/demo/HelloSecretController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloSecretController {
   @Value("${sm://greeting}")
-  String greeting="HI";
+  String greeting;
 
   @GetMapping("/")
   public String hello() {
