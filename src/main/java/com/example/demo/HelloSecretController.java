3f6f0ef88d2dc29b5d951015aa059ebb935f2550
Artem Bondar <artem.bondar@gmail.com>
2023-10-20 16:26:37 -0500

    initial setup

--- src/main/java/com/example/demo/HelloSecretController.java
+++ src/main/java/com/example/demo/HelloSecretController.java
@@ -1,18 +1,14 @@
 package com.example.demo;
 
-
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloSecretController {
-    @Value("${sm://petclinic-db-password}")
-    String greeting;
+  String greeting = "Hi";
 
   @GetMapping("/")
   public String hello() {
     return greeting + " World!";
   }
-    
 }
