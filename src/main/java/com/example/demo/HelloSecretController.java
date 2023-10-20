1c24d09be28d9507515b1515e3cf7e6b579d7f13
Artem Bondar <artem.bondar@gmail.com>
2023-10-20 16:31:20 -0500

    try again

--- src/main/java/com/example/demo/HelloSecretController.java
+++ src/main/java/com/example/demo/HelloSecretController.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloSecretController {
-  String greeting = "Hi";
+  @Value("${sm://petclinic-db-password}")
+  String greeting;
 
   @GetMapping("/")
   public String hello() {
